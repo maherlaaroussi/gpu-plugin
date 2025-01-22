135c4cbc5f3b0c7e17828f9ede1b57cdc05e6d65
Maher LAAROUSSI <maher.laaroussi@alten.com>
2025-01-22 19:31:30 +0100

    wip: gpu

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from src.backend.PluginManager.PluginBase import PluginBase
 from src.backend.PluginManager.ActionHolder import ActionHolder
 
 # Import actions
-from .actions.SimpleAction.SimpleAction import SimpleAction
+from .actions.GPU.Usage import Usage
 
 class PluginTemplate(PluginBase):
     def __init__(self):
@@ -12,16 +12,16 @@ class PluginTemplate(PluginBase):
         ## Register actions
         self.simple_action_holder = ActionHolder(
             plugin_base = self,
-            action_base = SimpleAction,
-            action_id = "dev_core447_Template::SimpleAction", # Change this to your own plugin id
-            action_name = "Simple Action",
+            action_base = Usage,
+            action_id = "com_maherlaaroussi_gpuplugin::Usage",
+            action_name = "GPU Usage",
         )
         self.add_action_holder(self.simple_action_holder)
 
         # Register plugin
         self.register(
-            plugin_name = "Template",
-            github_repo = "https://github.com/StreamController/PluginTemplate",
+            plugin_name = "GPU",
+            github_repo = "https://github.com/maherlaaroussi/gpu-plugin",
             plugin_version = "1.0.0",
             app_version = "1.1.1-alpha"
         )
